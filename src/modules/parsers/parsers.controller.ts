89ba51371a802cf570ff5a41dbfdad95cc505ad8
Alex Tim <buildmnt@gmail.com>
2023-01-30 21:23:52 +0200

    feat: selector & parser

--- src/modules/parsers/parsers.controller.ts
+++ src/modules/parsers/parsers.controller.ts
@@ -19,32 +19,32 @@ import { ParsersService } from './parsers.service';
 
 // @ApiCookieAuth()
 // @UseGuards(SelfGuard)
-@Controller('parsers')
+@Controller()
 export class ParsersController {
   constructor(private readonly service: ParsersService) {}
 
-  @Get()
-  async getParsers(@Query('selectorId', ParseIntPipe) selectorId: number) {
+  @Get('selectors/:selectorId/parsers')
+  async getParsers(@Param('selectorId', ParseIntPipe) selectorId: number) {
     return this.service.getParsers(selectorId);
   }
 
-  @Post('reorder')
+  @Post('parsers/reorder')
   async reorderParsers(@Body() dto: ReorderParsersDto) {
     const allResults = await this.service.reorderParsers(dto);
-    const ids: number[] = [];
+    const failed: number[] = [];
     allResults.forEach(({ affected }, i) => {
       if (!affected) {
-        ids.push(dto.items[i][0]);
+        failed.push(dto.items[i][0]);
       }
     });
-    if (ids.length) {
+    if (failed.length) {
       return new NotFoundException(
-        `Parsers with ids = [${ids.join(', ')}] are not found`,
+        `Parsers with ids = [${failed.join(', ')}] are not found`,
       );
     }
   }
 
-  @Get(':parserId')
+  @Get('selectors/:selectorId/parsers/:parserId')
   async getParser(@Param('parserId', ParseIntPipe) parserId: number) {
     const parser = await this.service.getParser(parserId);
     if (!parser) {
@@ -53,7 +53,7 @@ export class ParsersController {
     return parser;
   }
 
-  @Delete(':parserId')
+  @Delete('selectors/:selectorId/parsers/:parserId')
   async deleteParser(@Param('parserId', ParseIntPipe) parserId: number) {
     const deletedRecord = await this.service.deleteParser(parserId);
     if (!deletedRecord) {
@@ -62,12 +62,12 @@ export class ParsersController {
     return deletedRecord;
   }
 
-  @Post()
+  @Post('selectors/:selectorId/parsers')
   async createParser(@Body() dto: CreateParserDto) {
     return this.service.createParser(dto);
   }
 
-  @Patch(':parserId')
+  @Patch('selectors/:selectorId/parsers/:parserId')
   async updateParser(
     @Param('parserId', ParseIntPipe) parserId: number,
     @Body() dto: UpdateParserDto,
