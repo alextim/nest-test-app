a3eb4159bb53aa389924954e7cb7e62d3fa997e1
Alex Tim <buildmnt@gmail.com>
2023-03-02 16:07:11 +0200

    feat: seed

--- seed/typeorm-seed.config.ts
+++ seed/typeorm-seed.config.ts
@@ -7,23 +7,20 @@ import { SeedTimezone2617378125500 } from './timezone/SeedTimezone2617378125500'
 import { SeedSession2617378125501 } from './session/SeedSession2617378125501';
 
 config();
- 
+
 const configService = new ConfigService();
 
 const dataSourceOptions = {
-  ...getDataSourceOptions(configService) as any,
+  ...(getDataSourceOptions(configService) as any),
 
   host: configService.get<string>('DB_HOST'),
   port: configService.get<number>('DB_PORT'),
   database: configService.get<string>('DB_NAME'),
   username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),  
+  password: configService.get<string>('DB_PASSWORD'),
 
   entities: ['src/**/*.entity.ts'],
   autoLoadEntities: true,
-  migrations: [
-    SeedTimezone2617378125500,
-    SeedSession2617378125501
-  ],
+  migrations: [SeedTimezone2617378125500, SeedSession2617378125501],
 };
 export default new DataSource(dataSourceOptions);
