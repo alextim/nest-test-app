98b513aad2b47c311009801b8814f598a569476c
Alex Tim <buildmnt@gmail.com>
2023-03-04 00:01:09 +0200

    refactor: url

--- seed/typeorm-seed.config.ts
+++ seed/typeorm-seed.config.ts
@@ -1,17 +1,14 @@
 import { DataSource } from 'typeorm';
 import 'dotenv/config';
 
-import { CustomNamingStrategy } from '../src/lib/orm/CustomNamingStrategy';
+import { getDataSourceOptions } from '../src/lib/orm/datasource.options';
 
 import { CreateTimezone2617378125500 } from './timezone/CreateTimezone2617378125500';
 import { SeedTimezone2617378125502 } from './timezone/SeedTimezone2617378125502';
 import { CreateSession2617378125501 } from './session/CreateSession2617378125501';
 
 export default new DataSource({
-  type: 'postgres',
-  schema: 'public',
-  url: process.env.DATABASE_URL,
-  namingStrategy: new CustomNamingStrategy(),
+  ...getDataSourceOptions(),
 
   entities: ['src/**/*.entity.ts'],
   migrations: [
